dacba508e04a57c7cb7e15ac56e14a75d691ea7a
Your Name <you@example.com>
2022-09-29 17:56:40 +0700

    done

---
+++ SpringDataJPA/SpringDataJPAPractice/src/main/java/com/example/SpringDataJPAPractice/service/NhanVienService.java
@@ -0,0 +1,25 @@
+package com.example.SpringDataJPAPractice.service;
+
+import com.example.SpringDataJPAPractice.entity.NhanVien;
+
+import java.util.List;
+
+public interface NhanVienService {
+    List<NhanVien> getNhanViensByLuong();
+
+    Long getAllLuong();
+
+    List<String> getMaNhanVienLaiMayBay();
+
+    List<NhanVien> getNhanViensByChungNhan();
+
+    List<String> getMaNhanVienByLoaiMayBay();
+
+    List<String> getTenPhiCongLaiMayBayBoeing();
+
+    List<NhanVien> findNhanVienKhongPhaiPhiCong();
+
+    List<String> getMaNhanVienCoMucLuongCaoNhat();
+
+
+}
