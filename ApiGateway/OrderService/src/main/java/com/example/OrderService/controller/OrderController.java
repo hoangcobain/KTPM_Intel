51b4d667ed53bc32b3dd66a480852fe64e550ead
Your Name <you@example.com>
2022-11-04 10:21:18 +0700

    CircuitBreaker

--- ApiGateway/OrderService/src/main/java/com/example/OrderService/controller/OrderController.java
+++ ApiGateway/OrderService/src/main/java/com/example/OrderService/controller/OrderController.java
@@ -1,5 +1,6 @@
 package com.example.OrderService.controller;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,7 +10,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/api/order" )
 public class OrderController {
     @GetMapping
+    @CircuitBreaker(name = "inventory",fallbackMethod = "fallbackMethod")
     public String getOrder() {
         return "Hello from Order Service";
+    }
+
+    public String fallbackMethod(RuntimeException runtimeException) {
+        return "Something went wrong, please request order a next time";
     }
 }
